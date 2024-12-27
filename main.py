42d40c02a2dd17f0fd821646c92024563a58e82e
AndrewVreeland <Andrewvreeland@gmail.com>
2024-12-26 23:42:08 -0500

    I have moving asteroids

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from AsteroidField import AsteroidField
 def main():
     
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x,y)
+    asteroid_field = AsteroidField()
     
 
     print("Starting asteroids!")
